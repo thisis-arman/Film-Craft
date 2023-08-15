3174a2a0c551c6eea751ebba0a5996dc25477c16
thisis-arman <armanhossen1549@gmail.com>
2023-08-15 20:34:50 +0600

    become a instructor poster

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,35 +1,31 @@
-
-
-
-import Categories from '../../Components/Categories/Categories';
-import PopularClasses from '../../Components/PopularClasses/PopularClasses';
-import PopularInstructors from '../../Components/PopularInstructors/PopularInstructors';
-import Slider from './HomeComponents/Slider';
-import Testimonials from './HomeComponents/Testimonials';
-import ContactForm from './HomeComponents/ContactForm';
-import Offer from './HomeComponents/Offer';
-import BlogSection from './HomeComponents/BlogSection';
-import Category from '../../Components/Category/Category';
-import Companies from './HomeComponents/Companies';
+import Categories from "../../Components/Categories/Categories";
+import PopularClasses from "../../Components/PopularClasses/PopularClasses";
+import PopularInstructors from "../../Components/PopularInstructors/PopularInstructors";
+import Slider from "./HomeComponents/Slider";
+import Testimonials from "./HomeComponents/Testimonials";
+import ContactForm from "./HomeComponents/ContactForm";
+import Offer from "./HomeComponents/Offer";
+import BlogSection from "./HomeComponents/BlogSection";
+import Category from "../../Components/Category/Category";
+import Companies from "./HomeComponents/Companies";
+import BeInstructor from "./HomeComponents/BeInstructor";
 
 const Home = () => {
-    return (
-        <div>
-
-           <Slider/>
-           <Category/>
-           <PopularClasses/>
-           <Offer/>
-           <PopularInstructors/>
-           <Testimonials/>
-           <BlogSection/>
-           <ContactForm/>
-           <Categories/>
-           <Companies/>
-
-
-        </div>
-    );
+  return (
+    <div>
+      <Slider />
+      <Category />
+      <PopularClasses />
+      <Offer />
+      <PopularInstructors />
+      <Testimonials />
+      <BlogSection />
+      <ContactForm />
+      <BeInstructor />
+      <Categories />
+      <Companies />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Pages/Home/HomeComponents/BeInstructor.jsx
@@ -0,0 +1,98 @@
+const BeInstructor = () => {
+  return (
+    <section className=" container py-16 mx-auto grid md:grid-cols-2 grid-cols-1 items-center gap-4 w-full">
+      <div className="bg-gradient-to-t from-[#ae431f] to-[#FF6636] flex justify-between items-center  ">
+        <div className="p-4 text-white">
+          <h2 className="text-3xl font-bold py-4">Become An Instructor</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias maiores, odio at iusto ab id.</p>
+          <button className="btn-primary">Become Instructor</button>
+        </div>
+        <img className='w-44' src="https://i.ibb.co/BjT8C25/wepik-export-20230815142021l1hs.png" alt="" />
+      </div>
+
+      <div className="p-[29px] border">
+        <h2 className="text-3xl font-bold text-left pb-4 ">Your teaching and earning steps</h2>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="p-2 sm:max-w-sm w-full">
+            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+                <path d="M22 4L12 14.01l-3-3" />
+              </svg>
+              <span className="title-font font-medium">
+                Kinfolk Chips Snackwave
+              </span>
+            </div>
+          </div>
+          <div className="p-2 sm:max-w-sm w-full">
+            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+                <path d="M22 4L12 14.01l-3-3" />
+              </svg>
+              <span className="title-font font-medium">
+                Coloring Book Ethical
+              </span>
+            </div>
+          </div>
+          <div className="p-2 sm:max-w-sm w-full">
+            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+                <path d="M22 4L12 14.01l-3-3" />
+              </svg>
+              <span className="title-font font-medium">
+                Coloring Book Ethical
+              </span>
+            </div>
+          </div>
+          <div className="p-2 sm:max-w-sm w-full">
+            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+                <path d="M22 4L12 14.01l-3-3" />
+              </svg>
+              <span className="title-font font-medium">
+                Coloring Book Ethical
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BeInstructor;
