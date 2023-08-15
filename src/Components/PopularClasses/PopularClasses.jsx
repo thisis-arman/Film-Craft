1a46ec691ee3f9414d9ab2e78980e0ed10d17447
thisis-arman <armanhossen1549@gmail.com>
2023-08-15 14:20:23 +0600

    instructor section

--- src/Components/PopularClasses/PopularClasses.jsx
+++ src/Components/PopularClasses/PopularClasses.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import { FaStar } from "react-icons/fa";
+import { AiOutlineUser } from "react-icons/ai";
 
 
 const PopularClasses = () => {
@@ -21,30 +23,35 @@ const PopularClasses = () => {
     
     // const classes =JSON.parse(classesData).Classes;
     const sortedClass = classesData.sort((a, b) => b.enrolled - a.enrolled);
-    const popularClasses=(sortedClass).slice(0, 6);
+    const popularClasses=(sortedClass).slice(0, 8);
     
     // const popularClasses =popularClasses.slice(0,6)
     // //console.log('popular',popularClasses)
 
     return (
         <div data-aos="fade-up" data-aos-delay="200" className="w-full md:px-10 mx-auto px-4 ">
-            <h1 className="text-4xl font-bold text-center my-24">Popular Classes</h1>
+            <h1 className="text-4xl font-bold text-center mt-24 mb-8">Best Selling Courses</h1>
 
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 ">
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5 ">
                {
                 popularClasses.map(cls=>
                    
-                    <Link to={`/class/${cls._id}`} key={cls.serialNumber} className="card  hover:shadow-7xl hover:rounded-sm hover:shadow-slate-700 rounded-none  mx-auto xl:w-[400px] w-full h-[450px] bg-base-100 shadow-xl">
-                    <figure className="max-h-60"><img className="max-h-60 w-full" src={cls.image} alt={cls.ClassName} /></figure>
-                    <div className="p-4">
+                    <Link to={`/class/${cls._id}`} key={cls.serialNumber} className="card  hover:shadow-7xl hover:rounded-sm hover:shadow-slate-700 rounded-none  mx-auto  w-full pb-2 bg-base-100 shadow-xl">
+                    <figure className="max-h-44"><img className="max-h-60 w-full" src={cls.image} alt={cls.ClassName} /></figure>
+                    <p className="text-sm pl-4 flex justify-between p-2"><span className="bg-pink-100 p-1 rounded-md bg-opacity-60">{cls.level}</span> <span className="bg-green-100 p-1 rounded-md bg-opacity-60">{cls.category}</span> </p>
+                    <div className="px-4 ">
                         <p className="bg-white absolute top-0 right-0 m-4 p-1 px-2 rounded font-semibold">${cls.Price}</p>
-                        <h2 className="card-title font-bold text-2xl">{cls.ClassName}</h2>
-                        <p className="py-3">{cls.keyLearnings.brief}</p>
-                        <div>
-                        <p className="py-1 text-sm" >Enrolled: {cls.enrolled} students</p>
-                        <p className="py-1 text-sm" >Reviews: {cls.reviews} ({cls.numberOfReviews} +reviews)</p>
+                        <h2 className="card-title font-semibold text-xl">{cls.ClassName}</h2>
+                        <p className="py-3">{(cls.keyLearnings.brief).slice(0,50)}..</p>
+                        <hr />
+                        <div className="flex justify-between bottom-0 items-end relative">
+                        <p className="py-1  flex items-center gap-1 text-md" >
+                            <AiOutlineUser/> <span>
+                        {cls.enrolled} students</span></p>
+                        <p className="py-1 text-md flex gap-1 items-center" ><FaStar className="text-yellow-500"/> <span >
+                        {cls.reviews} ({cls.numberOfReviews} +reviews)</span></p>
                         </div>
-                       <p className="text-sm py-1">{cls.level} -{cls.category}- {cls.duration}</p>
+                      
                         <div className="card-actions justify-end">
                             {/* <button className="btn-primary">Buy Now</button> */}
                         </div>
--- src/Components/PopularInstructors/PopularInstructors.jsx
+++ src/Components/PopularInstructors/PopularInstructors.jsx
@@ -19,45 +19,29 @@ const PopularInstructors = () => {
     return (
         <div className="px-4">
             <section className=" body-font">
-  <div className="container w-5/6  py-24 mx-auto">
-    <div className="flex flex-col text-center w-full mb-20">
-      <h1 className="text-2xl font-bold title-font mb-4 ">OUR TOP INSTRUCTOR</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably  heard of them.</p>
+  <div className="container w-5/6 bg-white border my-24 p-8 mx-auto">
+    <div className="flex flex-col text-center w-full mb-10">
+      <h1 className="text-3xl font-bold title-font  ">TOP INSTRUCTOR OF THE MONTH</h1>
+     
     </div>
 
-    <div className= " w-full grid md:grid-cols-3 gap-4 grid-cols-1">
+    <div className= " w-full grid md:grid-cols-4 gap-4 grid-cols-1">
 
     {
         popularClasses.map((item,i)=>
         // console.log(i,item)
 
-        <div key={i} className="p-4 shadow-md  border">
-        <div className="h-full w-full flex  flex-col items-center text-center">
-          <img alt="team" className="flex-shrink-0 rounded-full h-36 w-36 mx-auto object-cover object-center mb-4" src={`${item?.photo}`} />
-          <div className="w-full">
-            <h2 className="title-font font-medium text-2xl">{item?.name}</h2>
-            <h2 className="title-font font-medium text-lg">{item?.email}</h2>
-            <p>Total Student: {item?.enrolled}</p>
-            
-            {/* <p className="mb-4">{(item?.background).slice(0,200)}..</p> */}
-            <span className="inline-flex">
-              <a className="t">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                </svg>
-              </a>
-              <a className="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                </svg>
-              </a>
-              <a className="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                </svg>
-              </a>
-            </span>
-          </div>
+        <div key={i} className=" shadow-md pb-4 border">
+        <div>
+          <img className="max-h-44 w-full" src={item?.photo} alt="" />
+        </div>
+        <div className="px-2 text-center">
+          <h2 className="text-center text-xl font-bold">{item?.name}</h2>
+          <p>Film Maker</p>
+        </div>
+        <hr />
+        <div className="px-4 text-center">
+          <p>{item?.email}</p>
         </div>
       </div>
         )
